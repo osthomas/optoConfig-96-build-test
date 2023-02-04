e7a313a275cdca7b875341e01cdeef89b0aa52ad
Oliver Thomas <thomaso@ie-freiburg.mpg.de>
2023-02-04 15:32:53 +0100

    Add script for creating a dmg from the .app

---
+++ create_dmg.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Create a .dmg archive from the .app after building
+create-dmg \
+    --volname "optoConfig" \
+    --volicon "optoConfig96/resources/oc96.icns" \
+    --window-size 600 300 \
+    --icon-size 100 \
+    --icon "optoConfig96.app" 175 120 \
+    --hide-extension "optoConfig96.app" \
+    --app-drop-link 425 120 \
+    optoConfig96.dmg dist/optoConfig96.app
